caba55ef1e301e36ee3622802eace12096031bc3
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-11-13 01:13:44 -0300

    corrigindo o erro de tipagem

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 
 export function CreateInvoice() {
@@ -28,10 +31,12 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const initialState = { message: null, errors: {} };
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const [state, dispatch] = useFormState(deleteInvoiceWithId, initialState);
  
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={dispatch}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-4" />
